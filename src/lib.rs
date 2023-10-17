1bd7fa390dd1082160586d4a4933d24ef71251ef
Wenbo Zhang <wenbo.zhang@iomesh.com>
2023-10-17 11:58:16 +0000

    feat(erpc-rs): export Server related structs
    Signed-off-by: Wenbo Zhang <wenbo.zhang@iomesh.com>

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ mod msg_buffer;
 mod nexus;
 mod req_handle;
 mod rpc;
+mod server;
 mod timely;
 mod timing_wheel;
 
@@ -22,55 +23,39 @@ pub mod prelude {
     #[doc(no_inline)]
     pub use crate::buf::MsgBufferReader;
     #[doc(no_inline)]
-    pub use crate::channel::{Channel, ChannelBuilder};
+    pub use crate::call::{CallTag, Codec, RpcCall};
+    #[doc(no_inline)]
+    pub use crate::channel::{Channel, ChannelBuilder, RpcContext};
     #[doc(no_inline)]
     pub use crate::client::Client;
     #[doc(no_inline)]
-    pub use crate::codec::*;
+    pub use crate::codec::{pr_codec::de as pr_de, pr_codec::ser as pr_ser, Marshaller};
     #[doc(no_inline)]
     pub use crate::env::{EnvBuilder, Environment};
     #[doc(no_inline)]
+    pub use crate::error::Result;
+    #[doc(no_inline)]
+    pub use crate::method::Method;
+    #[doc(no_inline)]
     pub use crate::msg_buffer::MsgBuffer;
     #[doc(no_inline)]
-    pub use crate::nexus::Nexus;
-    #[doc(no_inline)]
-    pub use crate::nexus::ReqHandler;
+    pub use crate::nexus::{Nexus, ReqHandler};
     #[doc(no_inline)]
     pub use crate::req_handle::ReqHandle;
     #[doc(no_inline)]
-    pub use crate::rpc::ContFunc;
+    pub use crate::rpc::{ContFunc, Rpc, SmHandler};
     #[doc(no_inline)]
-    pub use crate::rpc::Rpc;
-    #[doc(no_inline)]
-    pub use crate::rpc::SmHandler;
+    pub use crate::server::{Server, ServerBuilder, ServerRpcContext, Service, ServiceBuilder};
     #[doc(no_inline)]
     pub use crate::timely::Timely;
     #[doc(no_inline)]
     pub use crate::timing_wheel::TimingWheel;
     #[doc(no_inline)]
-    pub use erpc_sys::erpc::get_uri_for_process;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::kSessionCredits;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::rdtsc;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::sm_err_type_str;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::sm_event_type_str;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::to_usec;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::ChronoTimer;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::FastRand;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::MsgBuffer as RawMsgBuffer;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::ReqHandle as RawReqHandle;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::SmErrType;
-    #[doc(no_inline)]
-    pub use erpc_sys::erpc::SmEventType;
+    pub use erpc_sys::erpc::{
+        get_uri_for_process, kSessionCredits, rdtsc, sm_err_type_str, sm_event_type_str, to_usec,
+        ChronoTimer, FastRand, MsgBuffer as RawMsgBuffer, ReqHandle as RawReqHandle, SmErrType,
+        SmEventType,
+    };
     #[doc(no_inline)]
     pub use erpc_sys::{c_int, c_void, moveit, UniquePtr};
 }
